a4a59defb8c0847ca664f7363ffdfe7e1d24f2ba
Tim Sears <tim@timsears.com>
2024-03-01 15:18:26 -0800

    force provider type

--- src/assistants/character.assistant.ts
+++ src/assistants/character.assistant.ts
@@ -37,7 +37,7 @@ export const characterAssistant: CreateAssistantDTO = {
   },
     voice: {
         voiceId: "vits-ara-1",
-        provider: "neets" as "openai" | "azure" | "11labs" | "playht" | "rime-ai" | "deepgram" | "neets"
+        provider: "neets" as any
     },
     firstMessage: "مرحباً، أنا جروك. يمكنك التحدث معي كما لو كنت شخصًا. كيف يمكنني مساعدتك اليوم؟",
   // hipaaEnabled: true,
